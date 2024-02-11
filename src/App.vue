87580cd16b4cc09a984ba50ebc488759302f7a1c
ddavid930826@gmail.com <ddavid930826@gmail.com>
2024-02-11 17:59:23 +0100

    feat: separate logic from fab button

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,36 @@
 <template>
-  <div>
-    <UseDraggable
-      storage-key="vueuse-draggable-import-overrides"
-      storage-type="session"
-      v-slot="{ style }"
-    >
-      <Button
-        :style="style"
-        v-show="!isOpened"
-        @click="isOpened = true"
-        class="relative m-4 w-[50px] h-[50px] hover:animate-background-shine bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%]"
-      >
-        <p class="text-gray-300">{ ··· }</p>
-      </Button>
-    </UseDraggable>
-    <div v-show="isOpened" class="animate-expand-vertically absolute inset-x-0 bottom-96 h-16">
-      <Card>
-        <CardHeader>
-          <CardTitle>Import Map Overrides</CardTitle>
-          <CardDescription
-            >This developer tool allows you to view and override your import maps. Start by clicking
-            on a module that you'd like to override.
-          </CardDescription>
+  <FabButton :isOpen="isOpen" @close="isOpen = true" />
+  <div v-show="isOpen" class="animate-expand-vertically absolute inset-x-0 bottom-96 h-16">
+    <Card>
+      <CardHeader>
+        <CardTitle>Import Map Overrides</CardTitle>
+        <CardDescription
+          >This developer tool allows you to view and override your import maps. Start by clicking
+          on a module that you'd like to override.
+        </CardDescription>
 
-          <button
-            @click="mode = mode === 'dark' ? 'light' : 'dark'"
-            class="absolute right-12 top-4 rounded-sm opacity-70"
-            variant="outline"
-            size="icon"
-          >
-            <component :is="mode === 'dark' ? Moon : Sun" class="w-5 h-5 animate-fade-in" />
-          </button>
+        <button
+          @click="mode = mode === 'dark' ? 'light' : 'dark'"
+          class="absolute right-12 top-4 rounded-sm opacity-70"
+          variant="outline"
+          size="icon"
+        >
+          <component :is="mode === 'dark' ? Moon : Sun" class="w-5 h-5 animate-fade-in" />
+        </button>
 
-          <button
-            @click="isOpened = false"
-            class="absolute right-4 top-4 rounded-sm opacity-70"
-            variant="outline"
-            size="icon"
-          >
-            <Minus class="w-5 h-5" />
-          </button>
-        </CardHeader>
-        <CardContent>
-          <OverridesTable />
-        </CardContent>
-      </Card>
-    </div>
+        <button
+          @click="isOpen = false"
+          class="absolute right-4 top-4 rounded-sm opacity-70"
+          variant="outline"
+          size="icon"
+        >
+          <Minus class="w-5 h-5" />
+        </button>
+      </CardHeader>
+      <CardContent>
+        <OverridesTable />
+      </CardContent>
+    </Card>
   </div>
 </template>
 <script setup lang="ts">
@@ -53,10 +38,10 @@ import { Minus, Moon, Sun } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import OverridesTable from '@/components/OverridesTable.vue'
-import { ref } from 'vue'
+import { shallowRef } from 'vue'
 import { useColorMode } from '@vueuse/core'
-import { UseDraggable } from '@vueuse/components'
+import FabButton from '@/components/FabButton.vue'
 
 const mode = useColorMode()
-const isOpened = ref()
+const isOpen = shallowRef(false)
 </script>
---
+++ src/components/FabButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :style="style" class="fixed" ref="principalButton">
+    <Button
+      v-show="!isOpen"
+      @click="$emit('close')"
+      class="relative m-4 w-[50px] h-[50px] hover:animate-background-shine bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%]"
+    >
+      <p class="text-gray-300">{ ··· }</p>
+    </Button>
+  </div>
+</template>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { shallowRef, unref } from 'vue'
+import { useDraggable, type UseDraggableOptions, useStorage, watchDebounced } from '@vueuse/core'
+
+defineProps<{ isOpen: boolean }>()
+defineEmits(['close'])
+
+const principalButton = shallowRef()
+const stylesStorage = useStorage<UseDraggableOptions['initialValue']>(
+  'draggable-import-overrides',
+  { x: 0, y: 0 }
+)
+// x=left and y=top
+const { style, x, y } = useDraggable(principalButton, { initialValue: unref(stylesStorage.value) })
+
+watchDebounced([x, y], () => (stylesStorage.value = { x, y }), { debounce: 1500 })
+</script>
