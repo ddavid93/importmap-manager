d28b1d7380ad12c192daf6d76c60e79429fb51a9
ddavid930826@gmail.com <ddavid930826@gmail.com>
2024-12-27 17:01:26 +0100

    feat: prettier

--- src/App.vue
+++ src/App.vue
@@ -27,12 +27,19 @@
   </div>
 </template>
 <script setup lang="ts">
+/**
+ * Very important since this is the init of Overrides
+ */
+import '@/original/api/js-api'
 import { Minus, Moon, Sun } from 'lucide-vue-next'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import OverridesTable from '@/components/OverridesTable.vue'
 import { shallowRef } from 'vue'
 import { useColorMode } from '@vueuse/core'
 import FabButton from '@/components/FabButton.vue'
+
+const overrideMap = window.importMapOverrides.getOverrideMap();
+console.log(overrideMap);
 
 const mode = useColorMode()
 const isOpen = shallowRef(false)
