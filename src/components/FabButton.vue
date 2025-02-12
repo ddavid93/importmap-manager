daf1889307226cb35b374710bfe232cec5940bb3
matteo <matteo.cosi@live.it>
2025-02-12 21:04:07 +0100

    Refactor FabButton and add tooltip with keyboard shortcut support
    Reimplemented FabButton using UseDraggable for improved usability and added position constraints. Introduced a tooltip component with a "Ctrl+Alt+D" keyboard shortcut for toggling visibility. Included new Tooltip components and updated dependencies in package.json.

--- src/components/FabButton.vue
+++ src/components/FabButton.vue
@@ -1,29 +1,56 @@
 <template>
-  <div :style="style" ref="principalButton">
-    <Button
-      v-show="!isOpen"
-      @click="$emit('close')"
-      class="relative m-4 w-[50px] h-[50px] hover:animate-background-shine bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%]"
-    >
-      <p class="text-gray-300">{ ··· }</p>
-    </Button>
-  </div>
+  <UseDraggable
+    p="x-4 y-2"
+    v-show="!isOpen"
+    class="fixed cursor-move w-[50px] h-[50px] hover:animate-background-shine bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%]"
+    :initial-value="position"
+    prevent-default
+    :storage-key="storageKey"
+    storage-type="session"
+    :disabled="isDraggingDisabled"
+  >
+    <div class="cursor-move p-1 w-full h-full flex flex-align-center justify-center">
+      <div
+        class="w-full h-full flex items-center justify-center cursor-pointer"
+        @click="$emit('close')"
+        @mouseenter="isDraggingDisabled = true"
+        @mouseleave="isDraggingDisabled = false"
+      >
+        <p class="text-gray-300 select-none">{ ... }</p>
+      </div>
+    </div>
+  </UseDraggable>
 </template>
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { shallowRef, unref } from 'vue'
-import { useDraggable, type UseDraggableOptions, useStorage, watchDebounced } from '@vueuse/core'
+import { UseDraggable } from '@vueuse/components'
+import { computed, shallowRef } from 'vue'
+import { useStorage, useWindowSize, watchDebounced } from '@vueuse/core'
+
+const storageKey = 'dev-tools-fab-position'
 
 defineProps<{ isOpen: boolean }>()
 defineEmits(['close'])
 
-const principalButton = shallowRef()
-const stylesStorage = useStorage<UseDraggableOptions['initialValue']>(
-  'draggable-importmap-manager',
-  { x: 0, y: 0 }
-)
-// x=left and y=top
-const { style, x, y } = useDraggable(principalButton, { initialValue: unref(stylesStorage.value) })
+const isDraggingDisabled = shallowRef(false)
+const position = useStorage(storageKey, { x: 0, y: 0 })
 
-watchDebounced([x, y], () => (stylesStorage.value = { x: x.value, y: y.value }), { debounce: 1500 })
+const { width: windowWidth, height: windowHeight } = useWindowSize()
+
+const constrainPosition = computed(() => {
+  const elementWidth = 50
+  const elementHeight = 50
+
+  return {
+    x: Math.max(0, Math.min(position.value.x, windowWidth.value - elementWidth)),
+    y: Math.max(0, Math.min(position.value.y, windowHeight.value - elementHeight))
+  }
+})
+
+watchDebounced(
+  constrainPosition,
+  (value) => {
+    position.value = value
+  },
+  { debounce: 200 }
+)
 </script>
